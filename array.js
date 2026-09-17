d6fcb1d5d2cd9d108cbf0954c1c0942281c62734
labibmaxer <mdkhalidshaifullah54@gmail.com>
2026-09-17 16:35:53 +0600

    added new changes

--- array.js
+++ array.js
@@ -1,5 +1,6 @@
 const key = ['abc', 'cde', 'eghw' , 'dhaka', 'djao']
 for (i = 0 ; i < key.length ; i++){
-    console.log(key[i]);
-    
+    key.reverse();
+    console.log(key[i]);    
 }
+ 
---
+++ new.js
@@ -0,0 +1,12 @@
+const colors = ['red', 'blue', 'green', 'yellow', 'orange'];
+
+let reverse = [];
+for(i = colors.length -1 ; i >= 0 ; i--){
+    console.log(colors[i]);
+    
+    reverse.push(colors[i]);
+    
+}
+console.log(reverse);
+
+ 
